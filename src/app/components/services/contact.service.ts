54a91490142f47914eb85e6cb287aa3deef315da
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-10-10 21:38:20 +0100

    loaded-end-points

--- src/app/components/services/contact.service.ts
+++ src/app/components/services/contact.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IConatct } from '../models/IContact';
-import { throwError } from 'rxjs';
+import { IContact } from '../models/IContact';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,9 +14,44 @@ export class ContactService {
   public getAllContacts() {
     let dataURL: string = `${this.serverUrl}/contacts`;
 
-    return this.httpclient.get<IConatct[]>(dataURL).pipe();
+    return this.httpclient
+      .get<IContact[]>(dataURL)
+      .pipe(catchError(this.handleEroor));
+  }
+  // Get single Contact
+  public getContact(contactId: string): Observable<IContact> {
+    let dataURL: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpclient
+      .get<IContact>(dataURL)
+      .pipe(catchError(this.handleEroor));
   }
 
+  // Create Contact
+  public createContact(contact: IContact): Observable<IContact> {
+    let dataURL: string = `${this.serverUrl}/contacts`;
+    return this.httpclient
+      .post<IContact>(dataURL, contact)
+      .pipe(catchError(this.handleEroor));
+  }
+  // Update Contact
+  public updateContact(
+    contact: IContact,
+    contactId: string
+  ): Observable<IContact> {
+    let dataURL: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpclient
+      .put<IContact>(dataURL, contact)
+      .pipe(catchError(this.handleEroor));
+  }
+  // Delete Contact
+  public deleteContact(contactId: string): Observable<{}> {
+    let dataURL: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpclient
+      .delete<{}>(dataURL)
+      .pipe(catchError(this.handleEroor));
+  }
+
+  // Error Response Handling
   public handleEroor(error: HttpErrorResponse) {
     let errorMessage: String = '';
     if (error.error instanceof ErrorEvent) {
