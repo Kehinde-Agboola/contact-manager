5e296c9c16434a66b55f2c1c47756452386af746
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-10-11 20:12:16 +0100

    edit-profile-updates

--- src/app/components/edit-contact/edit-contact.component.ts
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { IContact } from '../models/IContact';
+import { IGroup } from '../models/IGroup';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-edit-contact',
@@ -7,4 +11,52 @@ import { Component } from '@angular/core';
 })
 export class EditContactComponent {
   profile = '../assets/profile.png';
+
+  public loading: boolean = false;
+  public contactId: string | null = null;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
+  public groups: IGroup[] = [] as IGroup[];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private contactService: ContactService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.activatedRoute.paramMap.subscribe((param: ParamMap) => {
+      this.contactId = param.get('contactId');
+    });
+    if (this.contactId) {
+      this.loading = true;
+      this.contactService.getContact(this.contactId).subscribe(
+        (data: IContact) => {
+          this.contact = data;
+          this.loading = false;
+          this.contactService.getAllGroups().subscribe((data: IGroup[]) => {
+            this.groups = data;
+          });
+        },
+        (error) => {
+          this.errorMessage = error;
+          this.loading = false;
+        }
+      );
+    }
+  }
+  submitUpdate() {
+    if (this.contactId) {
+      this.contactService.updateContact(this.contact, this.contactId).subscribe(
+        (data: IContact) => {
+          this.router.navigate(['/']).then();
+        },
+        (error) => {
+          this.errorMessage = error;
+          this.router.navigate(['/contacts/edit/${this.contactId}']).then();
+        }
+      );
+    }
+  }
 }
