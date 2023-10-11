5b5ad3c5919439011ceddc7e31d61ffb21586324
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-10-11 20:11:22 +0100

    users-updates

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { IContact } from '../models/IContact';
+import { IGroup } from '../models/IGroup';
+import { ContactService } from '../services/contact.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.scss']
+  styleUrls: ['./add-contact.component.scss'],
 })
 export class AddContactComponent {
+  public loading: boolean = false;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
+  public groups: IGroup[] = [] as IGroup[];
+  constructor(private contactService: ContactService, private router: Router) {
+    console.log(contactService);
+  }
 
+  ngOnInit(): void {
+    this.contactService.getAllGroups().subscribe(
+      (data: IGroup[]) => {
+        this.groups = data;
+        this.loading = false;
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.loading = false;
+      }
+    );
+  }
+  public createSubmit() {
+    this.contactService.createContact(this.contact).subscribe(
+      (data: IContact) => {
+        this.router.navigate(['/']).then();
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.router.navigate(['/contacts/add']).then();
+      }
+    );
+  }
 }
+// console.log(contact)
+// public isNotEmpty() {
+//   return Object.keys(this.contact).length > 0 && Object.keys(this.group);
+// }
+// }
